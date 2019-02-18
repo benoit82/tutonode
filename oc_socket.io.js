d63ea50f9805873630cdca55dfd87dd489bd7034
DUBUS Benoit <benoit.dubus@ext.soprasteria.com>
2019-02-18 10:05:35 +0100

    Fin de cours socket.io + entrainement chat

--- oc_socket.io.js
+++ oc_socket.io.js
@@ -15,11 +15,19 @@ var io = require('socket.io').listen(server);
 // Quand un client se connecte, on le note dans la console
 io.sockets.on('connection', (socket) => {
     socket.emit('message', 'Vous êtes bien connecté !');
-
+    //connection d'un client
+    socket.broadcast.emit('message', 'Un autre client vient de se connecter !');
+    //stockage du pseudo reçu
+    socket.on('petit_nouveau', (pseudo) => {
+        socket.pseudo = pseudo;
+    });
+    
     // Quand le serveur reçoit un signal de type "message" du client    
     socket.on('message', (message) => {
-        console.log('Un client me parle ! Il me dit : ' + message);
+        socket.emit('message', 'Tu as dit : ' + message);
+        socket.broadcast.emit('message', socket.pseudo + ' : ' + message);
     });
+    
 });
 
 
